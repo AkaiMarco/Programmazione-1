453d491fc31bd673adb07ec66530fca3b35ef37a
Orlando <orlando.murru@gmail.com>
2022-02-21 20:14:38 +0000

    -u

--- esempio-parentesi.cc
+++ esempio-parentesi.cc
@@ -1,4 +1,5 @@
-
+#include <iostream>
+#include <cstring>
 
 
 void init(char* translation_table)
@@ -6,7 +7,6 @@ void init(char* translation_table)
 	for(int i = 0; i < 256; ++i) {
 		translation_table[i] = i;
 	}
-
 	tranlastion_table['('] = '[';
 	tranlastion_table[')'] = ']';
 	tranlastion_table['['] = '{';
@@ -14,4 +14,23 @@ void init(char* translation_table)
 	tranlastion_table['{'] = '(';
 	tranlastion_table['}'] = ')';
 }
+
+int main(int argc, char* argv[])
+{
+	char translation_table[256];
+
+	init(translation_table);
+
+	if (argc !=  2) {
+		return 1;
+	}
+
+	for(char *p = argv[1] ; *p; ++p) {
+		*p = translation_table[(int)(*p)];
+	}
+
+	std::cout << p << std::endl;
+	return 0;
+}
+
 
